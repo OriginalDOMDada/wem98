19cec4db730ef354d75727b7ec00736d637b368e
William Mannie <wmannie@Will-Mannies-MacBook-Pro.local>
2017-10-19 14:17:29 -0400

    push for lasss nite

--- src/components/windows/Aboutme.vue
+++ src/components/windows/Aboutme.vue
@@ -1,7 +1,7 @@
 <template>
 <div style="height: 100vw; width: 100vw; position: relative;">
 <transition-group name="fade"  tag="div" class="windows">
-<vue-draggable-resizable :h="400" :w="400" v-if="appData.applications.aboutme.openApp" v-bind:name="appData.applications.aboutme.text" v-bind:open="appData.applications.aboutme.openApp" v-bind:key="appData.applications.aboutme" id="aboutme" class="box-md app">
+<vue-draggable-resizable :h="400" :w="400" v-if="appData.applications.aboutme.openApp" v-bind:name="appData.applications.aboutme.text" v-bind:open="appData.applications.aboutme.openApp" v-bind:key="1" id="aboutme" class="box-md app">
 <div class="big-rap">
 <div class="box-header">
     <div class="title-box">
@@ -16,7 +16,7 @@
   </div>
     <div class="wrappa-da-rappa">
         <div class="app-content" name="aboutme">
-          <p>Hello,<br> My name is <a href="www.google.com">William Mannie</a>, I am a magical unicorn that likes to create really cool shit. I currently blaze the streets of Washinton, D.C., where by day I am the official #wintern at nclud, a creative desin agency. To maintain my survial I like eating bomb ass food, taking long trots and, hunting the internet for awesomeness. My areas of expertiese lie within fron-end design/devlopment and illustrating. Check out my portforlio and see what I've been up to lately.</p>
+          <p>Hello,<br> My name is <a @mouseover='mannie' @mouseleave='before' href='#'>William Mannie</a>, I am a magical <a @mouseover='unicorn' @mouseleave='before' href="#">unicorn</a> that likes to <a @mouseover='create' @mouseleave='before' href="#">create</a> really <a @mouseover='cool' @mouseleave='before' href="#">cool</a> <a @mouseover='shit' @mouseleave='before' href="#">shit</a>. I currently <a @mouseover='blaze' @mouseleave='before' href="#">blaze</a> the streets of <a @mouseover='dc' @mouseleave='before' href="#">Washinton, D.C.</a>, where by day I am <a @mouseover='webdev' @mouseleave='before' href="#">web developer</a> at Adfro. To maintain my <a  @mouseover='survival' @mouseleave='before' href="#">survial</a> i eat.. <a @mouseover='alot' @mouseleave='before' href="#">a lot</a>. But in my spare time, i enjoy riding my <a @mouseover='bike' @mouseleave='before' href="#">bike</a> and <a  @mouseover='hunting' @mouseleave='before' href="#">hunting</a> the <a @mouseover='internet' @mouseleave='before' href="#">internet</a> for <a @mouseover='awesomeness' @mouseleave='before' href="#">awesomeness</a>. My areas of expertiese lie within fron-end design/devlopment and illustrating. Check out my site and see what I've been up to <a @mouseover='lately' @mouseleave='before' href="#">lately</a>.</p>
         </div>
       </div>
   </div>
@@ -35,6 +35,88 @@ export default {
   data: function () {
     return {
       appData
+    }
+  },
+  methods: {
+    mannie: function () {
+      document.getElementsByClassName('panel')['0'].style.WebkitAnimation = 'inherit'
+      document.getElementsByClassName('panel')['0'].style.backgroundImage = `url(${require('../../assets/gifs/keef.gif')})`
+      document.getElementsByClassName('panel')['0'].style.backgroundSize = 'cover'
+    },
+    unicorn: function () {
+      document.getElementsByClassName('panel')['0'].style.WebkitAnimation = 'inherit'
+      document.getElementsByClassName('panel')['0'].style.backgroundImage = `url(${require('../../assets/gifs/unicorn.gif')})`
+      document.getElementsByClassName('panel')['0'].style.backgroundSize = 'inital'
+    },
+    create: function () {
+      document.getElementsByClassName('panel')['0'].style.WebkitAnimation = 'inherit'
+      document.getElementsByClassName('panel')['0'].style.backgroundImage = `url(${require('../../assets/gifs/lego.gif')})`
+      document.getElementsByClassName('panel')['0'].style.backgroundSize = 'cover'
+    },
+    cool: function () {
+      document.getElementsByClassName('panel')['0'].style.WebkitAnimation = 'inherit'
+      document.getElementsByClassName('panel')['0'].style.backgroundImage = `url(${require('../../assets/gifs/cool.gif')})`
+      document.getElementsByClassName('panel')['0'].style.backgroundSize = 'cover'
+    },
+    shit: function () {
+      document.getElementsByClassName('panel')['0'].style.WebkitAnimation = 'inherit'
+      document.getElementsByClassName('panel')['0'].style.backgroundImage = `url(${require('../../assets/gifs/shit.gif')})`
+      document.getElementsByClassName('panel')['0'].style.backgroundSize = 'cover'
+    },
+    blaze: function () {
+      document.getElementsByClassName('panel')['0'].style.WebkitAnimation = 'inherit'
+      document.getElementsByClassName('panel')['0'].style.backgroundImage = `url(${require('../../assets/gifs/blaze.gif')})`
+      document.getElementsByClassName('panel')['0'].style.backgroundSize = 'cover'
+    },
+    dc: function () {
+      document.getElementsByClassName('panel')['0'].style.WebkitAnimation = 'inherit'
+      document.getElementsByClassName('panel')['0'].style.backgroundImage = `url(${require('../../assets/gifs/dc.gif')})`
+      document.getElementsByClassName('panel')['0'].style.backgroundSize = 'contain'
+    },
+    webdev: function () {
+      document.getElementsByClassName('panel')['0'].style.WebkitAnimation = 'inherit'
+      document.getElementsByClassName('panel')['0'].style.backgroundImage = `url(${require('../../assets/gifs/webdev.gif')})`
+      document.getElementsByClassName('panel')['0'].style.backgroundSize = 'inital'
+    },
+    bike: function () {
+      document.getElementsByClassName('panel')['0'].style.WebkitAnimation = 'inherit'
+      document.getElementsByClassName('panel')['0'].style.backgroundImage = `url(${require('../../assets/gifs/bike.gif')})`
+      document.getElementsByClassName('panel')['0'].style.backgroundSize = 'inital'
+    },
+    survival: function () {
+      document.getElementsByClassName('panel')['0'].style.WebkitAnimation = 'inherit'
+      document.getElementsByClassName('panel')['0'].style.backgroundImage = `url(${require('../../assets/gifs/survival.gif')})`
+      document.getElementsByClassName('panel')['0'].style.backgroundSize = 'cover'
+    },
+    alot: function () {
+      document.getElementsByClassName('panel')['0'].style.WebkitAnimation = 'inherit'
+      document.getElementsByClassName('panel')['0'].style.backgroundImage = `url(${require('../../assets/gifs/alot.gif')})`
+      document.getElementsByClassName('panel')['0'].style.backgroundSize = 'inital'
+    },
+    hunting: function () {
+      document.getElementsByClassName('panel')['0'].style.WebkitAnimation = 'inherit'
+      document.getElementsByClassName('panel')['0'].style.backgroundImage = `url(${require('../../assets/gifs/hunting.gif')})`
+      document.getElementsByClassName('panel')['0'].style.backgroundSize = 'cover'
+    },
+    internet: function () {
+      document.getElementsByClassName('panel')['0'].style.WebkitAnimation = 'inherit'
+      document.getElementsByClassName('panel')['0'].style.backgroundImage = `url(${require('../../assets/gifs/internet.gif')})`
+      document.getElementsByClassName('panel')['0'].style.backgroundSize = 'cover'
+    },
+    awesomeness: function () {
+      document.getElementsByClassName('panel')['0'].style.WebkitAnimation = 'inherit'
+      document.getElementsByClassName('panel')['0'].style.backgroundImage = `url(${require('../../assets/gifs/awesomeness.gif')})`
+      document.getElementsByClassName('panel')['0'].style.backgroundSize = 'inital'
+    },
+    lately: function () {
+      document.getElementsByClassName('panel')['0'].style.WebkitAnimation = 'inherit'
+      document.getElementsByClassName('panel')['0'].style.backgroundImage = `url(${require('../../assets/gifs/lately.gif')})`
+      document.getElementsByClassName('panel')['0'].style.backgroundSize = 'cover'
+    },
+    before: function () {
+      document.getElementsByClassName('panel')['0'].style.WebkitAnimation = 'slide 15s linear infinite'
+      document.getElementsByClassName('panel')['0'].style.backgroundImage = ''
+      document.getElementsByClassName('panel')['0'].style.backgroundSize = ''
     }
   }
 }
